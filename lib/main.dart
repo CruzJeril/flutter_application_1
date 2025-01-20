d14670aa29ff7e4d6feb782946976d90e7d84a07
CruzJeril <jeril.cruz13@gmail.com>
2025-01-20 14:13:45 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            "This is my first mobile app!", 
+          style: GoogleFonts.getFont(
+            'Praise',
+            textStyle: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            )
+          ),
+          )
+          ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () { },
+          child: const Text('click'),
+          backgroundColor: Colors.red[600],
+        ),
+    );
+  }
+}
